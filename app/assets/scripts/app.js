4f78aaa590147761558734d03f6a37cb226009f2
Cassandra Dauphin <cdauphindev@gmail.com>
2020-11-29 05:36:49 -0500

    Complete large hero block and intro content block

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,6 @@
+import '../styles/main.css'
+
+
+if(module.hot) {
+    module.hot.accept()
+}
---
+++ dist/webpack.config.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var path = require('path');
+
+var postCSSPlugins = [require('postcss-import'), require('postcss-mixins'), require('postcss-simple-vars'), require('postcss-nested'), require('autoprefixer')];
+/** @type {import('@types/webpack').Configuration} */
+
+module.exports = {
+  entry: './app/assets/scripts/app.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  devServer: {
+    before: function before(app, server) {
+      server._watch('./app/**/*.html');
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader', {
+        loader: "postcss-loader",
+        options: {
+          postcssOptions: {
+            plugins: postCSSPlugins
+          }
+        }
+      }]
+    }]
+  }
+};
